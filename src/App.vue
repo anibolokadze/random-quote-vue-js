ddcf3e9702bd28391a5ef2cb6ba2217f97f1df3c
dimitribolokadze <89190087+anibolokadze@users.noreply.github.com>
2023-03-28 21:30:14 +0400

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <main>
     <Quotes />
-  </div>
+  </main>
 </template>
 
 <script>
@@ -14,4 +14,25 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-shadow: border-box;
+  font-family: "Manrope", sans-serif;
+}
+:root {
+  --LightCyan: hsl(193, 38%, 86%);
+  --Neon-Green: hsl(150, 100%, 66%);
+  --Grayish-Blue: hsl(217, 19%, 38%);
+  --Dark-Grayish-Blue: hsl(217, 19%, 24%);
+  --Dark-Blue: hsl(218, 23%, 16%);
+}
+main {
+  background: var(--Dark-Blue);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,14 +1,23 @@
 <template>
-  <h1>Quotes</h1>
+  <div class="wrapper">
+    <p>Quote</p>
 
-  <div v-if="loading" class="loading">Searching...</div>
+    <div v-if="loading" class="loading">Searching...</div>
 
-  <div v-if="quote.error">
-    <p>{{ quote.error }}</p>
+    <div v-if="quote.error">
+      <p>{{ quote.error }}</p>
+    </div>
+
+    <div>
+      <h1>{{ quote.content }}</h1>
+    </div>
+
+    <div class="divider"></div>
+
+    <button @click="getRandomQuote">
+      <img src="../assets/icon-dice.svg" />
+    </button>
   </div>
-
-  <div>{{ quote.content }}</div>
-  <button @click="getRandomQuote">click</button>
 </template>
 <script>
 import axios from "axios";
@@ -42,3 +51,29 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.wrapper {
+  background: var(--Dark-Grayish-Blue);
+  width: 80%;
+  height: 300px;
+  border-radius: 20px;
+  color: var(--LightCyan);
+  padding: 20px;
+}
+p {
+  color: var(--Neon-Green);
+}
+h1 {
+  font-size: 20px;
+}
+button {
+  all: unset;
+}
+.divider {
+  background-image: url("../assets/pattern-divider-mobile.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 20px;
+}
+</style>
